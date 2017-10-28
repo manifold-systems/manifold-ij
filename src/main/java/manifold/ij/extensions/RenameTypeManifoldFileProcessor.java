4198f8a5af1a7b7b37a077a19b250526fd09b0d5
scott <rsmckinney@hotmail.com>
2017-10-27 18:54:17 -0700

    support manifold type rename refactoring from a type reference in an editor (in addition to from the resource file in the project view)

--- src/main/java/manifold/ij/extensions/RenameTypeManifoldFileProcessor.java
+++ src/main/java/manifold/ij/extensions/RenameTypeManifoldFileProcessor.java
@@ -2,18 +2,23 @@ package manifold.ij.extensions;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileSystemItem;
 import com.intellij.psi.PsiPolyVariantReference;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.refactoring.listeners.RefactoringElementListener;
+import com.intellij.refactoring.rename.RenameDialog;
 import com.intellij.refactoring.rename.RenamePsiFileProcessor;
 import com.intellij.refactoring.rename.RenameUtil;
+import com.intellij.refactoring.ui.NameSuggestionsField;
 import com.intellij.refactoring.util.MoveRenameUsageInfo;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.util.Query;
@@ -30,6 +35,58 @@ import org.jetbrains.annotations.Nullable;
 public class RenameTypeManifoldFileProcessor extends RenamePsiFileProcessor
 {
   private List<UsageInfo> _usages = Collections.emptyList();
+
+  @Override
+  public boolean canProcessElement( @NotNull PsiElement element )
+  {
+    return super.canProcessElement( maybeGetResourceFile( element ) );
+  }
+
+  @Override
+  public boolean isInplaceRenameSupported()
+  {
+    return false;
+  }
+
+  @Nullable
+  @Override
+  public PsiElement substituteElementToRename( PsiElement element, @Nullable Editor editor )
+  {
+    return maybeGetResourceFile( element );
+  }
+
+  @Override
+  public RenameDialog createRenameDialog( Project project, PsiElement element, PsiElement nameSuggestionContext, Editor editor )
+  {
+    return new PsiFileRenameDialog( project, element, nameSuggestionContext, editor ) {
+      protected void createNewNameComponent() {
+        super.createNewNameComponent();
+        getNameSuggestionsField().selectNameWithoutExtension();
+      }
+
+      @Override
+      protected NameSuggestionsField getNameSuggestionsField()
+      {
+        return super.getNameSuggestionsField();
+      }
+    };
+  }
+
+  private PsiElement maybeGetResourceFile( PsiElement element )
+  {
+    if( element instanceof ManifoldPsiClass )
+    {
+      // Must refactor the corresponding resource file...
+
+      List<PsiFile> rawFiles = ((ManifoldPsiClass)element).getRawFiles();
+      if( rawFiles.size() == 1 )
+      {
+        //## todo; ?
+        element = rawFiles.get( 0 );
+      }
+    }
+    return element;
+  }
 
   @NotNull
   @Override
