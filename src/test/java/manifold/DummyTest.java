146e2c151e4b41af4f4a9d55650203a0314f71a7
Kyle Moore <kyle@manifold.systems>
2017-12-13 13:07:22 -0800

    Works for me...

---
+++ src/test/java/manifold/DummyTest.java
@@ -0,0 +1,11 @@
+package manifold;
+
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+public class DummyTest extends LightCodeInsightFixtureTestCase {
+
+  public void testImportsLightCodeInsightFixtureTestCase() {
+    fail("intentional failure");
+  }
+
+}
