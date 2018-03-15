b0c7456b20da534aad2cbe00c6ccf3201f0b13b9
scott <rsmckinney@hotmail.com>
2018-03-14 18:19:42 -0700

    maybe fix timing issue when running ImageRenameTest on ci

--- src/test/java/manifold/ij/image/ImageRenameTest.java
+++ src/test/java/manifold/ij/image/ImageRenameTest.java
@@ -1,6 +1,7 @@
 package manifold.ij.image;
 
 import com.intellij.psi.PsiClass;
+import java.awt.EventQueue;
 import manifold.ij.AbstractManifoldCodeInsightTest;
 import manifold.ij.util.SettleModalEventQueue;
 
@@ -11,6 +12,7 @@ public class ImageRenameTest extends AbstractManifoldCodeInsightTest
     myFixture.copyFileToProject( "image/sample/Logo.png" );
     myFixture.configureByFile( "image/rename/TestImageRename.java" );
 
+    EventQueue.invokeLater( () -> {} );
     myFixture.renameElementAtCaret( "Logo2.png" );
 
     // let remaining ui event processing finish (rename uses invokeLater())
@@ -25,6 +27,7 @@ public class ImageRenameTest extends AbstractManifoldCodeInsightTest
     myFixture.copyFileToProject( "image/sample/Logo.png" );
     myFixture.configureByFile( "image/rename/TestImageRename_NoCaret.java" );
 
+    EventQueue.invokeLater( () -> {} );
     PsiClass logoClass = myFixture.findClass( "image.sample.Logo_png" );
     myFixture.renameElement( logoClass.getContainingFile(), "Logo2.png" );
 
