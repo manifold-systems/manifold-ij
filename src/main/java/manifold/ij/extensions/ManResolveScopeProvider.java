e9e50f10121d9828ff4f4a7b7b4961a133a1c72d
smckinney <scott@manifold.systems>
2018-08-06 19:15:40 -0700

    - implement ResolveScopeEnlarger instead of ResolveScopeProvider as the extensions' scope may not subsume the scope of the extended type/file - create a union of scopes reflecting the modules that extend a type

--- src/main/java/manifold/ij/extensions/ManResolveScopeProvider.java
+++ src/main/java/manifold/ij/extensions/ManResolveScopeProvider.java
@@ -1,13 +1,16 @@
 package manifold.ij.extensions;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiClassOwnerEx;
+import com.intellij.psi.PsiClassOwner;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
-import com.intellij.psi.ResolveScopeProvider;
+import com.intellij.psi.ResolveScopeEnlarger;
 import com.intellij.psi.search.GlobalSearchScope;
 import java.util.Set;
+
+import com.intellij.psi.search.SearchScope;
 import manifold.api.type.ContributorKind;
 import manifold.api.type.ITypeManifold;
 import manifold.ij.core.ManModule;
@@ -20,26 +23,21 @@ import manifold.ij.core.ManProject;
  * Basically we need to use the scope of the Extension class, not the extended class.
  * This is mostly so any generic classes referenced in the extension class can resolve properly.
  */
-public class ManResolveScopeProvider extends ResolveScopeProvider
+public class ManResolveScopeProvider extends ResolveScopeEnlarger
 {
   @Override
-  public GlobalSearchScope getResolveScope( VirtualFile file, Project project )
+  public SearchScope getAdditionalResolveScope( VirtualFile file, Project project )
   {
     ManProject manProject = ManProject.manProjectFrom( project );
     PsiFile psiFile = PsiManager.getInstance( project ).findFile( file );
-    if( psiFile instanceof PsiClassOwnerEx )
+    GlobalSearchScope unionScope = null;
+    if( psiFile instanceof PsiClassOwner )
     {
-      PsiClassOwnerEx classFile = (PsiClassOwnerEx)psiFile;
-      for( String name : classFile.getClassNames() )
+      PsiClassOwner classFile = (PsiClassOwner)psiFile;
+      String fqn = classFile.getPackageName() + '.' + FileUtil.getNameWithoutExtension( classFile.getName() );
+      for( ManModule module : manProject.getModules() )
       {
-        String fqn = classFile.getPackageName() + '.' + name;
-        for( ManModule module : manProject.getModules() )
-        {
-          if( isTypeExtendedInModule( fqn, module ) )
-          {
-            return GlobalSearchScope.moduleWithDependenciesAndLibrariesScope( module.getIjModule() );
-          }
-        }
+        unionScope = addScopeIfExtended( fqn, unionScope, module );
       }
     }
     else
@@ -49,15 +47,31 @@ public class ManResolveScopeProvider extends ResolveScopeProvider
         String[] fqns = module.getTypesForFile( manProject.getFileSystem().getIFile( file ) );
         for( String fqn: fqns )
         {
-          if( isTypeExtendedInModule( fqn, module ) )
-          {
-            return GlobalSearchScope.moduleWithDependenciesAndLibrariesScope( module.getIjModule() );
-          }
+          unionScope = addScopeIfExtended( fqn, unionScope, module );
         }
       }
-
     }
-    return null;
+    return unionScope;
+  }
+
+  private GlobalSearchScope addScopeIfExtended( String fqn, GlobalSearchScope unionScope, ManModule module )
+  {
+    if( isTypeExtendedInModule( fqn, module ) )
+    {
+      GlobalSearchScope scope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope( module.getIjModule() );
+      if( unionScope == null )
+      {
+        unionScope = scope;
+      }
+      else
+      {
+        if( !scope.isSearchInModuleContent( module.getIjModule() ) )
+        {
+          unionScope = unionScope.union( scope );
+        }
+      }
+    }
+    return unionScope;
   }
 
   private boolean isTypeExtendedInModule( String fqn, ManModule module )
