48f5383f2a33523e157b820d7d1e5daa7cc9fb35
scott <rsmckinney@hotmail.com>
2019-10-14 15:32:06 -0700

    must reset fields earlier than ProjectComponent scope, otherwise intellij get references to the original value and stuff goes to shit later. e.g., STRING_LITERAL reference must be ours everywhere from square one.

--- src/main/java/manifold/ij/core/ManProjectComponent.java
+++ src/main/java/manifold/ij/core/ManProjectComponent.java
@@ -10,22 +10,14 @@ import com.intellij.ide.startup.impl.StartupManagerImpl;
 import com.intellij.lang.Language;
 import com.intellij.lang.LanguageAnnotators;
 import com.intellij.lang.LanguageBraceMatching;
-import com.intellij.lang.java.parser.JavaParser;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.impl.java.stubs.JavaLiteralExpressionElementType;
-import com.intellij.psi.impl.java.stubs.JavaStubElementTypes;
-import com.intellij.psi.impl.source.tree.JavaElementType;
-import com.intellij.psi.tree.IElementType;
-import java.util.function.Supplier;
-import manifold.ij.extensions.ManJavaLiteralExpressionElementType;
 import manifold.ij.extensions.ManifoldPsiClassAnnotator;
 import manifold.ij.template.ManTemplateBraceMatcher;
 import manifold.ij.template.ManTemplateLanguage;
-import manifold.util.ReflectUtil;
 
 public class ManProjectComponent implements ProjectComponent
 {
@@ -78,42 +70,10 @@ public class ManProjectComponent implements ProjectComponent
           // All this stuff is at the "Application" scope, but we have to initialize the first time a Project loads
           StartupManagerImpl.getInstance( _project ).registerStartupActivity( () -> {
             registerAnnotatorWithAllLanguages();
-            replaceJavaExpressionParser();
-            overrideJavaStringLiterals();
           } );
         }
       }
     }
-  }
-
-  private void replaceJavaExpressionParser()
-  {
-    ReflectUtil.field( JavaParser.INSTANCE, "myExpressionParser" ).set( new ManExpressionParser( JavaParser.INSTANCE ) );
-    ReflectUtil.field( JavaElementType.BINARY_EXPRESSION, "myConstructor" ).set( (Supplier)ManPsiBinaryExpressionImpl::new );
-    ReflectUtil.field( JavaElementType.PREFIX_EXPRESSION, "myConstructor" ).set( (Supplier)ManPsiPrefixExpressionImpl::new );
-
-    ReflectUtil.field( JavaParser.INSTANCE, "myStatementParser" ).set( new ManStatementParser( JavaParser.INSTANCE ) );
-  }
-
-  /**
-   * Override Java String literals to handle fragments
-   */
-  private void overrideJavaStringLiterals()
-  {
-    ManJavaLiteralExpressionElementType override = new ManJavaLiteralExpressionElementType();
-    ReflectUtil.field( JavaStubElementTypes.class, "LITERAL_EXPRESSION" ).setStatic( override );
-
-    ApplicationManager.getApplication().runReadAction( () -> {
-      IElementType[] registry = (IElementType[])ReflectUtil.field( IElementType.class, "ourRegistry" ).getStatic();
-      for( int i = 0; i < registry.length; i++ )
-      {
-        if( registry[i] instanceof JavaLiteralExpressionElementType )
-        {
-          // ensure the original JavaLiteralExpressionElementType is replaced with ours
-          registry[i] = override;
-        }
-      }
-    } );
   }
 
   private void registerAnnotatorWithAllLanguages()
