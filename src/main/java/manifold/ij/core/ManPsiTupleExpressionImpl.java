4995aeb0c4c61f357e3078ef99bbfa56f53f30c4
scott <rsmckinney@hotmail.com>
2022-06-28 21:39:59 -0400

    integrate from master

--- src/main/java/manifold/ij/core/ManPsiTupleExpressionImpl.java
+++ src/main/java/manifold/ij/core/ManPsiTupleExpressionImpl.java
@@ -70,6 +70,12 @@ public class ManPsiTupleExpressionImpl extends ExpressionPsiElement implements M
     {
       return null;
     }
+
+    if( !ManProject.isManifoldInUse( this ) )
+    {
+      return null;
+    }
+
     String pkg = ManClassUtil.getPackage( topLevelClass.getQualifiedName() );
     String tupleTypeName = ManPsiUtil.runInTypeManifoldLoader( this,
       () -> ITupleTypeProvider.INSTANCE.get().makeType( pkg, makeTupleFieldMap() ) );
