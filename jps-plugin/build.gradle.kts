0b3bed4b497576cd203e97b2028b319c77fdaf98
scott <scott@manifold.systems>
2025-03-19 22:49:31 -0400

    https://github.com/manifold-systems/manifold/issues/680 - ensure JPS plugin targets Java 8 bytecode

--- jps-plugin/build.gradle.kts
+++ jps-plugin/build.gradle.kts
@@ -31,8 +31,8 @@ configurations {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
-  targetCompatibility = JavaVersion.VERSION_17
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 fun getIjVersion() : String {
